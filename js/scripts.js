f5d1c628827d1df42bf2717009596ae22f342139
telpuhova <telpuhova@gmail.com>
2017-11-27 10:32:48 -0800

    add other operations

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,19 @@ var add = function(number1, number2) {
 	return number1 + number2;
 };
 
+var substract = function(number1, number2) {
+  return number1 - number2;
+}
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+}
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+}
+
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
-alert(add(number1,number2));
+alert(multiply(number1,number2));
